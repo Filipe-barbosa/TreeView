54dd27469632f40715682b614dd0c22886e2d69a
filipe-barbosa <barbosasfilipe@gmail.com>
2023-11-25 12:53:43 -0300

    fix path alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { raise } from './shared/exceptions.ts';
+import App from '@/App';
+import '@/index.css';
+import { raise } from '@/shared/exceptions.ts';
 
 ReactDOM.createRoot(
   document.getElementById('root') ?? raise('root not found'),
